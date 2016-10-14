c9d1e5fe49222f41d0c303822c5ab38f6a46cd10
eanplatter <ean.platter@gmail.com>
2016-10-14 13:19:52 -0500

    womp

--- app.rb
+++ app.rb
@@ -1,9 +1,38 @@
 require 'sinatra'
 require 'sinatra/json'
 
-user = { "First Name" => "Ean", "Last Name" => "Plato" }
+class User
+  def initialize(first_name, last_name)
+    @first_name = first_name
+    @last_name = last_name
+    @id = rand(1..100000)
+  end
+  def first_name
+    @first_name
+  end
+  def last_name
+    @last_name
+  end
+  def id
+    @id
+  end
+  def details
+    @deets = { :first => @first_name, :surname => @last_name, :id => @id }
+  end
+end
 
-get '/' do
-  json user
+user = User.new('Ean', 'Plato')
+
+before do
+  request.body.rewind
+  @request_payload = JSON.parse request.body.read
+end
+
+get '/user' do
+  json user.details
+end
+
+post '/user' do
+  json @request_payload
 end
 
